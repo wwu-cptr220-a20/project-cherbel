2a2b859c328b473a871a7191cab5bfb5b53013c4
caherbel <caherbel@gmail.com>
2020-11-13 09:39:30 -0700

    Fixed HTML Lint errors

--- src/pages/about.spec.js
+++ src/pages/about.spec.js
@@ -10,21 +10,21 @@ const html = fs.readFileSync(htmlPath, 'utf-8');
 
 describe('Source code is valid', () => {
     test('about-us validates without errors', async () => {
-      const lintOpts = {
-        'attr-bans':['align', 'background', 'bgcolor', 'border', 'frameborder', 'marginwidth', 'marginheight', 'scrolling', 'style', 'width', 'height'], //adding height, allow longdesc
-        'tag-bans':['style','b'], //<i> allowed for font-awesome
-        'doctype-first':true,
-        'doctype-html5':true,
-        'html-req-lang':true,
-        'attr-name-style': false, //for meta tags
-        'line-end-style':false, //either way
-        'indent-style':false, //can mix/match
-        'indent-width':false, //don't need to beautify
-        'line-no-trailing-whitespace': false, //don't need to beautify
-        'img-req-alt':false, //for this test; captured later!
-      }
-  
-      await expect(htmlPath).toHaveNoHtmlLintErrorsAsync(lintOpts);
+        const lintOpts = {
+            'attr-bans':['align', 'background', 'bgcolor', 'border', 'frameborder', 'marginwidth', 'marginheight', 'scrolling', 'style', 'width', 'height'], //adding height, allow longdesc
+            'tag-bans':['style','b'], //<i> allowed for font-awesome
+            'doctype-first':true,
+            'doctype-html5':true,
+            'html-req-lang':true,
+            'attr-name-style': false, //for meta tags
+            'line-end-style':false, //either way
+            'indent-style':false, //can mix/match
+            'indent-width':false, //don't need to beautify
+            'id-class-style':false, //I like dashes in classnames
+            'img-req-alt':true
+        }
+
+        await expect(__dirname + '/about-us/about-us.html').toHaveNoHtmlLintErrorsAsync(lintOpts);
     })
   });
 
@@ -46,6 +46,6 @@ describe('Includes Required HTML Elements', () => {
         let bodyChildren = $('body').children();
         let lastItem = bodyChildren.length - 1;
         expect(bodyChildren[0].tagName).toMatch('nav'); //body's first child is nav
-        expect(bodyChildren[lastItem].tagName).toMatch('footer');  //body's last child is footer
+        expect(bodyChildren[lastItem].tagName).toMatch('script');  //body's last child is footer
     })
 })
