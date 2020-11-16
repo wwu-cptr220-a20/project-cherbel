9c7b06d10ac4f52b1b6fef5fc95ca015491fb6d7
caherbel <caherbel@gmail.com>
2020-11-15 23:27:00 -0700

    About Us is correct

--- src/components/pages/about-us/AboutUs.js
+++ src/components/pages/about-us/AboutUs.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../../css/custom-default.css';
-import './AboutUs.css';
 import '../../css/fancy-buttons.css'
+import './AboutUs.css';
 
 class AboutUs extends Component {
 
@@ -10,15 +10,15 @@ class AboutUs extends Component {
             <main className="text-center">
                 <h1>About Us</h1>
                 <section>
-                    <h2 id="mission">Our Mission</h2>
+                    <h2>Our Mission</h2>
                     <p>
                         Through business development we will assist organizations to become independent from
                         non-profits by supporting sustainable projects and quality educational facilities that
                         will ultimately lead to children enjoying independent lives.
                     </p>
                 </section>
-                <section class="btn">
-                    <button class="donate-btn" type="button" onclick="window.location.href='../donate/donate.html';">Donate Now</button>
+                <section className="btn">
+                    <button className="donate-btn" type="button">Donate Now</button>
                 </section>
                 <section>
                     <h2>History</h2>
@@ -26,13 +26,13 @@ class AboutUs extends Component {
                 </section>
                 <section>
                     <h2 id="leadership">Leadership</h2>
-                    <div  class="flex-container">
-                        <div class="flex-item">
-                            <img class="first" src="https://i.ibb.co/yhT00M4/portrait-657116-1920.jpg" alt="A photo of _____"></img>
+                    <div  className="flex-container">
+                        <div className="flex-item">
+                            <img className="first" src="https://i.ibb.co/yhT00M4/portrait-657116-1920.jpg" alt="_____'s portrait"/>
                             <p>A Picture of ____ the _____ of our organization</p>
                         </div>
-                        <div class="flex-item">
-                            <img class="second" src="https://i.ibb.co/CHm3tDN/human-1411499-1920.jpg" alt="A photo of ____"></img>
+                        <div className="flex-item">
+                            <img className="second" src="https://i.ibb.co/CHm3tDN/human-1411499-1920.jpg" alt="____'s portrait"/>
                             <p>A Picture of ____ the _____ of our organization</p>
                         </div>
                     </div>
