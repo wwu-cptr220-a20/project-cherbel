5783efe070c111342a2ed10417f7919769bdb820
caherbel <caherbel@gmail.com>
2020-11-19 15:01:47 -0700

    Cleanup Master

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           <div>
               <NavBar />
               <Switch>
